ab4c2aa7be01a48a74e4567d990e6c0b746e790f
kixixixixi <kixixixixi@gmail.com>
2026-03-12 16:47:38 +0900

    Add ogp

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,24 @@ import "./reset.css"
 export const metadata = {
   description: "ScentifAI® is the Digital Olfaction AI Platform.",
   title: "Scentif.ai",
+  openGraph: {
+    description: "ScentifAI® is the Digital Olfaction AI Platform.",
+    images: [
+      {
+        url: "https://scentif.ai/images/logo_1.png",
+      },
+    ],
+    siteName: "Scentif.ai",
+    title: "Scentif.ai",
+    type: "website",
+    url: "https://scentif.ai",
+  },
+  twitter: {
+    card: "summary_large_image",
+    description: "ScentifAI® is the Digital Olfaction AI Platform.",
+    images: ["https://scentif.ai/images/logo_1.png"],
+    title: "Scentif.ai",
+  },
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
